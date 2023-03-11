99bd1e849e78d811fc36542c69c336c3f5458d1a
frontenddevproject <frontenddevproject@gmail.com>
2023-03-11 15:58:04 +0200

    search 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,34 @@ const ageInput = document.querySelector("#age");
 const cityInput = document.querySelector("#city");
 const createButton = document.querySelector("#create");
 const usersSection = document.querySelector("#users-section");
+const searchInput = document.querySelector("#search");
+const searchButton = document.querySelector("#search-button");
+const cancelSearchButton = document.querySelector("#cancel-search-button")
 
-let users = [];
+let users = [
+  { name: "Igor", city: "Kyiv", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
+  { name: "Igor", city: "Kyiv", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
+  { name: "Igor", city: "Kyiv", age: 20 },
+  { name: "Alex", city: "Kyiv", age: 50 },
+  { name: "Oleg", city: "Kyiv", age: 10 },
+];
+
+renderUsers (users);
 
 const deleteUser = (indexOfUser) => {
    users = users.filter((el, i) => i!== indexOfUser);
-   renderUsers();
+   renderUsers(users);
 }
 
-function renderUsers () {
+function renderUsers (usersToRender) {
 
    usersSection.innerHTML = "";
 
-   const usersContent = users.map((user) => `<div class="user-card">
+   const usersContent = usersToRender.map((user) => `<div class="user-card">
       <span>name:</span><span> ${user.name}</span>
       <span>age:</span><span> ${user.age}</span>
       <span>city:</span><span> ${user.city}</span>
@@ -39,11 +54,19 @@ function renderUsers () {
      
       const user = {name, age, city};
       users.push(user);
-
       nameInput.value = "";
       ageInput.value = "";
       cityInput.value = "";
 
-      renderUsers();
+      renderUsers(users);
    
+}
+searchButton.onclick = () => {
+   const usersToRender = users.filter((user) => user.name.includes(searchInput.value));
+   renderUsers(usersToRender);
+}
+
+cancelSearchButton.onclick = () =>{
+   renderUsers(users);
+   searchInput.value = "";
 }
